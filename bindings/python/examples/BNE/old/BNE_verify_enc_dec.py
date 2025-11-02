fbd00d266d415fa13a774d8b68e7e8bb1f7decb3
Marius <marius.tomek@outlook.com>
2025-11-02 22:47:27 +0100

    ran verification, success

--- bindings/python/examples/BNE/old/BNE_verify_enc_dec.py
+++ bindings/python/examples/BNE/old/BNE_verify_enc_dec.py
@@ -10,12 +10,30 @@ from tqdm import tqdm
 dataset = datasets.load_dataset("JeanKaddour/minipile", split="train")
 
 # Load tokenizer
-tokenizer = Tokenizer.from_file("examples/BNE/old/data/bne-byte-level_wt103.json")
-tokenizer.pre_tokenizer = ByteLevel()
+tokenizer = Tokenizer.from_file("examples/BNE/old/data/bne_byteLevel_minipile_5_tokens_16000.json")
+#tokenizer = Tokenizer.from_file("examples/BNE/old/data/bpe_byte-level_minipile.json")
+#tokenizer.pre_tokenizer = ByteLevel()
 tokenizer.decoder = ByteLevel()
 
 for text in tqdm(dataset["text"]):
-    #print(text)
-    assert(tokenizer.decode(tokenizer.encode(text).ids) == text)
+    #print(len(text)) 1620
+    #print(len(tokenizer.decode(tokenizer.encode(text).ids)[1:]))
+    #print("====== end ======")
+    if (len(text) < len(tokenizer.decode(tokenizer.encode(text).ids))):
+        #assert(tokenizer.decode(tokenizer.encode(text).ids)[1:] == text)
+        if(tokenizer.decode(tokenizer.encode(text).ids)[1:] != text):
+            print("====== orig ======")
+            print(text)
+            print("====== enc ======")
+            print(tokenizer.decode(tokenizer.encode(text).ids)[1:])
+            print("====== end ======")
+    else:
+        #assert(tokenizer.decode(tokenizer.encode(text).ids) == text)
+        if(tokenizer.decode(tokenizer.encode(text).ids) != text):
+            print("====== orig ======")
+            print(text)
+            print("====== enc ======")
+            print(tokenizer.decode(tokenizer.encode(text).ids))
+            print("====== end ======")
     #print(tokenizer.encode(text).ids)
     #print(tokenizer.encode(text).tokens)
